8ba5dde582a8460d5707795393956ec33347fb30
Brent <Bmilan24@gmail.com>
2017-06-23 19:48:38 -0400

    ReviewApp added

---
+++ src/main/java/production/ReviewApp.java
@@ -0,0 +1,15 @@
+package production;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import production.ReviewApp;
+
+@SpringBootApplication
+public class ReviewApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewApp.class, args);
+	}
+
+}
