5e366394fd6338a2ba54107723e4141c6837262c
Brent <Bmilan24@gmail.com>
2017-06-25 14:45:12 -0400

    images added, home link added on secondary pages, synopsis

--- src/main/java/production/ReviewRepository.java
+++ src/main/java/production/ReviewRepository.java
@@ -16,13 +16,13 @@ public class ReviewRepository {
 	public ReviewRepository() {
     
 	//Constructor
-	Review review = new Review(12345, "Saving Private Ryan", "url", "Drama", "content", "5/7/2012", "A bunch of guys save another guy in WWII", "tag");
+	Review review = new Review(12345, "No Country For Old Men", "/images/no-country.jpg", "Drama", "content", "5/7/2012", "If you come home and Javier Bardem is sitting in your living room, you're going to have a bad time.", "tag");
 	reviewMap.put(review.getId(),review);
-	Review review2 = new Review(13579, "The Lego Movie", "Lego URL", "Comedy", "content", "3/15/2012", "Everything is awesome", "tag");
+	Review review2 = new Review(13579, "There Will Be Blood", "/images/there-will-be.jpg", "Drama", "content", "3/15/2012", "Life is terrible. People are terrible. Daniel Day-Lewis can act.", "tag");
 	reviewMap.put(review2.getId(),review2);
-	Review review3 = new Review(17777, "Batman", "/images/batman.jpg", "Drama", "content", "5/7/2012", "Angry oprhan takes justice into his own hands. His only weakness: due process.", "tag");
+	Review review3 = new Review(17777, "Batman: The Dark Knight", "/images/joker.jpg", "Action/Drama", "content", "5/7/2012", "Angry oprhan takes justice into his own hands. His only weakness: due process.", "tag");
 	reviewMap.put(review3.getId(),review3);
-	Review review4 = new Review(10999, "The Big Lebowski", "/images/dude.jpg", "Drama", "content", "6/24/2017", "The Greatest Story Ever Told. A man and his rug try to do the impossible: abide.", "tag");
+	Review review4 = new Review(10999, "The Big Lebowski", "/images/dude.jpg", "Comedy/Fine Art", "content", "6/24/2017", "The Greatest Story Ever Told. A man and his rug try to do the impossible: abide.", "tag");
 	reviewMap.put(review4.getId(), review4);
 	
 	}	
